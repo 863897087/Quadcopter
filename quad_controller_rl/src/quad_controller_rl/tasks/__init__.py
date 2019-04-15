85fce4a5bbf7ccd525088e7a0682260d4ad7f260
robond <suny>
2019-04-15 05:24:36 -0700

    add LANDING task and ALL task mode

--- quad_controller_rl/src/quad_controller_rl/tasks/__init__.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/__init__.py
@@ -1,3 +1,5 @@
 from quad_controller_rl.tasks.base_task import BaseTask
 from quad_controller_rl.tasks.takeoff import Takeoff
 from quad_controller_rl.tasks.takehover import HOVER
+from quad_controller_rl.tasks.takelanding import LANDING
+from quad_controller_rl.tasks.takeall import ALL
