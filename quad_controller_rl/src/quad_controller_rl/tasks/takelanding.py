3752f9703e68c12d64992527c914005492a7d340
robond <suny>
2019-04-20 17:00:04 -0700

    reward mode set relative position and position z is 0.2 then reward=timestamp

--- quad_controller_rl/src/quad_controller_rl/tasks/takelanding.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/takelanding.py
@@ -9,12 +9,14 @@ class LANDING(BaseTask):
             np.array(
                 [0.0, 0.0, 0.0,
                  0.0, 0.0, 0.0, 0.0,
-                 0.0, 0.0, 0.0, 0.0]
+                 0.0, 0.0, 0.0, 0.0,
+                 0.0, 0.0, 0.0]
             ),
             np.array(
                 [0.0, 0.0, 0.0,
                  0.0, 0.0, 0.0, 0.0,
-                 0.0, 0.0, 0.0, 0.0]
+                 0.0, 0.0, 0.0, 0.0,
+                 0.0, 0.0, 0.0]
             )
         )
 
@@ -23,15 +25,15 @@ class LANDING(BaseTask):
             np.array([ 25.0,  25.0,  25.0,  25.0,  25.0,  25.0])
         )
 
+        self.star_pose = Point(0.0, 0.0, 10.0)
+        self.save_pose = Point(0.0, 0.0, 10.0)
         self.target_pose = Point(0.0, 0.0, 10.0)
-        self.down_time = 10
 
     def reset(self):
-        #self.agent.LANDING()
+        self.agent.LANDING()
 
-        self.target_pose = Point(0.0, 0.0, np.random.normal(10, 0.5))
         return Pose(
-            position=self.target_pose,
+            position=Point(0.0,0.0,10.0),
             orientation=Quaternion(0.0, 0.0, 0.0, 0.0)
         ), Twist(
             linear=Vector3(0.0, 0.0, 0.0),
@@ -42,30 +44,29 @@ class LANDING(BaseTask):
         Reward = 0.0
         Done = False
 
-        self.target_pose.z = (self.target_pose.z / self.down_time) * (self.down_time - timestamp)
+        self.target_pose = Point( self.star_pose.x, self.star_pose.y, self.star_pose.z * (1-timestamp/10) )
 
         State = np.array(
             [
                 (pose.position.x), (pose.position.y), (pose.position.z),
                 (pose.orientation.x), (pose.orientation.y), (pose.orientation.z), (pose.orientation.w),
-                (self.target_pose.x), (self.target_pose.y), (self.target_pose.z), timestamp
+                (self.target_pose.x), (self.target_pose.y), (self.target_pose.z), timestamp,
+                (linear_acceleration.x), (linear_acceleration.y), (linear_acceleration.z)
             ]
         )
 
-        Reward -= \
-            min(abs(pose.position.x - self.target_pose.x), 20) + \
-            min(abs(pose.position.y - self.target_pose.y), 20) + \
-            min(abs(pose.position.z - self.target_pose.z), 20)
-        Reward *= 0.1
+        Reward -= min(abs(pose.position.z - self.save_pose.z), 20)    +\
+                  min(abs(pose.position.x - self.save_pose.x), 20)*0.1+\
+                  min(abs(pose.position.y - self.save_pose.y), 20)*0.1
 
-        if pose.position.z <= 0.1:
+        self.save_pose = Point( pose.position.x, pose.position.y, pose.position.z * (1-timestamp/10) )
+
+        if pose.position.z <= 0.2:
+            Reward += timestamp
             Done = True
-            if self.down_time - timestamp <= 0:
-                Reward += 20
-            else:
-                Reward -= 10
 
-        if timestamp >= 15:
+        if timestamp >= 11 or abs(pose.position.x) > 50 or abs(pose.position.y) > 50 or abs(pose.position.z) > 50:
+            Reward -= 20
             Done = True
 
         action = self.agent.step(State, Reward, Done)
